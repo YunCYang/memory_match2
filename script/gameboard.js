bd9f78c3fc8dac533c82ecdc2a91354a3013599e
YunCYang <ericeorl2013@gmail.com>
2019-12-07 23:58:12 -0800

    finished difficulty modal page, working on team selection modal page, successfully retrieved api information

--- script/gameboard.js
+++ script/gameboard.js
@@ -18,14 +18,22 @@ class GameBoard {
     }
     this.gameMode = ["random", "draft", "custom"];
     this.gameModeHint = ["Use a random Pokemon team for a battle!", "Draft team from groups of random Pokemon!", "Pick your own team and battle!"];
+    this.difficulty = ["easy", "normal", "hard", "inferno"];
+    this.type = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel",
+      "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"]
     this.selectedGameMode = null;
     this.selectedTeamSize = null;
+    this.selectedDifficulty = null;
 
     this.handleMenu = this.handleMenu.bind(this);
     this.handleSettings = this.handleSettings.bind(this);
     this.closeSelectionModal = this.closeSelectionModal.bind(this);
+    this.handleGameModeReturn = this.handleGameModeReturn.bind(this);
+    this.handleDifficultyReturn = this.handleDifficultyReturn.bind(this);
     this.gameModeSelection = this.gameModeSelection.bind(this);
     this.teamSizeSelection = this.teamSizeSelection.bind(this);
+    this.difficultySelection = this.difficultySelection.bind(this);
+    this.requestPokemon = this.requestPokemon.bind(this);
   }
 
   addEventHandlers(element, elementEvent) {
@@ -96,7 +104,7 @@ class GameBoard {
     }).append($('<div>', { class: 'game_mode_head sub_selection_head' }).append($('<p>').text("Game Mode")),
       $('<div>', { class: 'team_size_head sub_selection_head' }).append($('<p>').text("Team Size")));
 
-    let modalContent = $('.selection_modal_content').append(selectionHead);
+    const modalContent = $('.selection_modal_content').append(selectionHead);
     const modalFooter = $('<div>', {
       class: 'modal_footer'
     });
@@ -128,19 +136,141 @@ class GameBoard {
       }).append(selectionBtnContainer.append(gameModeBtnContainer, teamSizeBtnContainer), hintContainer));
     }
     this.boardElement.selectionModalArea.append(modalContent.append(modalFooter));
+    this.removeEvent($('.game_mode_button_container > button'));
     this.addEventHandlers($('.selection_modal .close'), this.closeSelectionModal);
     this.addEventHandlers($('.game_mode_button_container > button'), this.gameModeSelection);
     this.removeEvent(this.boardElement.menuButton);
   }
 
   closeSelectionModal() {
-    $('.selection_modal').removeClass('pop_left').empty();
+    this.boardElement.selectionModalArea.removeClass('pop_left').empty();
     this.removeEvent($('.selection_modal .close'));
     this.addEventHandlers(this.boardElement.menuButton, this.handleMenu);
   }
 
   createTeamRosterMenu() {
-    $('.selection_modal').empty();
+    this.boardElement.selectionModalArea.empty();
+    const returnIcon = $('<div>', {
+      class: 'modal_header'
+    }).append($('<img>', {
+      class: 'return button',
+      src: './media/images/icon/return.png'
+    }));
+    const selectionModalContent = $('<div>', {
+      class: 'selection_modal_content overflow'
+    }).append(returnIcon);
+    this.boardElement.selectionModalArea.append(selectionModalContent);
+
+    const selectedTeamArea = $('<div>', {
+      class: 'selected_team_container'
+    });
+    for (var i = 0; i < 6; i++) {
+      selectedTeamArea.append($('<div>', {
+        class: 'selected_team_' + i + ' selected_team_slot',
+        selectedTeam: i
+      }))
+    }
+
+    const teamMainArea = $('<div>', {
+      class: 'team_main_container selection_button_container'
+    });
+    for (var j = 0; j < 18; j++) {
+      teamMainArea.append($('<div>', {
+        class: 'type_' + j + ' type_slot',
+        type: j
+      }).append($('<button>', {
+        class: 'type_button button type_button_' + j,
+        type: j
+      }).text(this.type[j])))
+    }
+
+    const confirmButtonArea = $('<div>', {
+      class: 'confirm_button_container selection_button_container'
+    }).append($('<button>', {
+      class: 'team_confirm_button button'
+    }).text("Game Start"));
+    const modalFooter = $('<div>', {
+      class: 'modal_footer'
+    });
+    selectionModalContent.append(selectedTeamArea, confirmButtonArea, teamMainArea, modalFooter);
+    this.removeEvent($('.return'));
+    this.addEventHandlers($('.return'), this.handleDifficultyReturn);
+    this.removeEvent($('.type_button'));
+    this.addEventHandlers($('.type_button'), this.requestPokemon);
+    //click on types will go to new function
+    //use different css to indicate team size
+    //general css
+    //responsive design
+  }
+
+  createDifficultyMenu() {
+    this.boardElement.selectionModalArea.empty();
+    const returnIcon = $('<div>', {
+      class: 'modal_header'
+    }).append($('<img>', {
+      class: 'return button',
+      src: './media/images/icon/return.png'
+    }));
+    const selectionModalContent = $('<div>', {
+      class: 'selection_modal_content'
+    }).append(returnIcon);
+    this.boardElement.selectionModalArea.append(selectionModalContent);
+
+    const difficultyHead = $('<div>', {
+      class: 'selection_head'
+    }).append($('<div>', { class: 'difficulty_head' }).append($('<p>').text("Difficulty")));
+    const modalContent = $('.selection_modal_content').append(difficultyHead);
+    const modalFooter = $('<div>', {
+      class: 'modal_footer'
+    });
+
+    for (let i = 0; i < this.difficulty.length; i++) {
+      let sectionClass = "selection_menu_" + i + " difficulty_section";
+      let difficultyClass = "difficulty_" + i + " button";
+
+      let selectionBtnContainer = $('<div>', {
+        class: 'selection_button_container'
+      }).append($('<button>', { class: difficultyClass, difficulty: i }).text(this.difficulty[i].toUpperCase()));
+
+      modalContent.append($('<div>', {
+        class: sectionClass
+      }).append(selectionBtnContainer));
+    }
+    this.boardElement.selectionModalArea.append(modalContent.append(modalFooter));
+    this.removeEvent($('.difficulty_section button'));
+    this.addEventHandlers($('.difficulty_section button'), this.difficultySelection);
+    this.removeEvent($('.return'));
+    this.addEventHandlers($('.return'), this.handleGameModeReturn);
+  }
+
+  handleGameModeReturn() {
+    //try to modify this portion of code later
+    this.boardElement.selectionModalArea.empty();
+    const closeIcon = $('<div>', {
+      class: 'modal_header'
+    }).append($('<span>', {
+      class: 'close',
+    }).html('&times;'));
+    const selectionModal = $('<div>', {
+      class: 'selection_modal_content'
+    }).append(closeIcon);
+    this.boardElement.selectionModalArea.append(selectionModal);
+    this.createGameModeMenu();
+    if (this.selectedGameMode) {
+      for (var i = 0; i < this.gameMode.length; i++) {
+        if (this.gameMode[i] === this.selectedGameMode) {
+          $('.game_mode_button_container > .game_mode_' + i).addClass('choice');
+          $('.team_size_button_container > .game_mode_' + i).removeClass('hide');
+        }
+      }
+    }
+    this.removeEvent($('.team_size_button_container > button'));
+    this.addEventHandlers($('.team_size_button_container > button'), this.teamSizeSelection);
+  }
+
+  handleDifficultyReturn() {
+    this.boardElement.selectionModalArea.empty();
+    this.createDifficultyMenu();
   }
 
   removeEvent(element) {
@@ -163,6 +293,41 @@ class GameBoard {
     this.selectedTeamSize = parseInt(teamSizeNumber);
     $('.team_size_button_container > button').removeClass('choice');
     $(event.currentTarget).addClass('choice');
+    this.createDifficultyMenu();
+  }
+
+  difficultySelection() {
+    const difficultyNumber = $(event.currentTarget).attr('difficulty');
+    this.selectedDifficulty = this.difficulty[difficultyNumber];
     this.createTeamRosterMenu();
+  }
+
+  requestPokemon() {
+    const typeIndex = $(event.currentTarget).attr('type');
+    const typeUrl = "https://pokeapi.co/api/v2/type/" + (parseInt(typeIndex) + 1);
+    var ajaxConfigObject = {
+      dataType: "json",
+      url: typeUrl,
+      method: "GET",
+      success: this.processGetPokemonData,
+      error: this.processGetPokemonError
+    };
+    $.ajax(ajaxConfigObject);
+  }
+
+  processGetPokemonData(response) {
+    console.log(response);
+    //get type pokemon length
+    //create length amount of divs
+    //get pokemon sprites and insert into divs
+    //name array or key, sprite array or value
+    //css
+    //add event handlers
+    //adjust return icon
+  }
+
+  processGetPokemonError(response) {
+    console.log(response);
+    alert("Error: cannot retrieve type information.")
   }
 }
